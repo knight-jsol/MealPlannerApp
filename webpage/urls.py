1efba3819cc2cbbff269383166722761b517fb8f
NValentine-UWM <valent46@uwm.edu>
2023-10-05 22:42:14 -0500

    Awk

--- webpage/urls.py
+++ webpage/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("/create_recipe", views.create_recipe, name="create_recipe"),
-    path("/pantry", views.create_recipe, name="pantry"),
-    path("/cart", views.create_recipe, name="cart")
+    path("/pantry", views.pantry, name="pantry"),
+    path("/cart", views.cart, name="cart")
 ]
