d7b6861d65940fe6b136431eae6dd24e9036ba80
twsewart <89991835+twsewart@users.noreply.github.com>
2023-11-09 18:39:42 -0600

    Update settings.py

--- capstone/settings.py
+++ capstone/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'tommy123',
+        'PASSWORD': 'ilovejrock',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
