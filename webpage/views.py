94e1be360fa6f1c2b7bb9a6023390505b9f445b3
ILoveJRock <valenat01@gmail.com>
2023-12-21 16:11:28 -0600

    Add signup functionality to the website

--- webpage/views.py
+++ webpage/views.py
@@ -20,6 +20,19 @@ from .forms import *
 from .models import *
 from django.contrib.auth import get_user_model, logout
 from openai import OpenAI
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login_view')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
 
 
 def logout_view(request):
